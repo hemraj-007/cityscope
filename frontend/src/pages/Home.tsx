d47ad651be09bb82e06d7b469d82f36b36831f92
hemraj-007 <140829250+hemraj-007@users.noreply.github.com>
2025-05-26 11:49:34 +0530

    fixed build errors

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,9 @@
-import { useEffect, type ReactNode } from 'react';
-import { useNavigate } from 'react-router-dom';
+import {  type ReactNode } from 'react';
+//import { useNavigate } from 'react-router-dom';
 import { Sparkles, MapPin, MessagesSquare } from 'lucide-react';
 
 const Home = () => {
-  const navigate = useNavigate();
+  //const navigate = useNavigate();
 
   // useEffect(() => {
   //   const token = localStorage.getItem('token');
